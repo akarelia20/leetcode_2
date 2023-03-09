9038911f8fe2b1677d11e6f6b4bdcb8c5036d6eb
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2023-03-08 20:32:10 -0800

    Time: 8519 ms (5.01%), Space: 28.8 MB (87.79%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -17,28 +17,3 @@ class Solution:
                 break      
                 
         return longest
-            
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         unique= set(nums)
-#         long = 0
-#         for i in nums:
-#             if (i-1) not in unique:
-#                 length = 1
-#                 while (i+length) in unique:
-#                     length += 1
-#                 long = max(length, long)
-#         return long
-            
