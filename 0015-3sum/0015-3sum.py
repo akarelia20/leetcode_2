48602895c74069797843cac1ab38774c28c28c3f
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2023-03-10 02:05:33 -0800

    Time: 2668 ms (28.03%), Space: 18.9 MB (11.73%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        unique = set()
+        res = []
+        
+        for n in range(len(nums)):
+            l,r = n+1, len(nums)-1 
+            while l < r:
+                if (nums[n]+ nums[l] + nums[r]) > 0:
+                    r -=1
+                elif (nums[n]+ nums[l] + nums[r]) < 0:
+                    l+= 1
+                elif (nums[n]+ nums[l] + nums[r]) == 0:
+                    y = "".join(str(j) for j in [nums[n], nums[l], nums[r]])
+                    if y not in unique:
+                        res.append([nums[n], nums[l], nums[r]])
+                    unique.add(y)
+                    r -= 1
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+       
+        return res
+                
+    
