2663b7960142262879e4f74de0ec41a3527f48cf
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2023-03-10 02:22:56 -0800

    Time: 1595 ms (46.93%), Space: 18.5 MB (22.71%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        unique = set()
         res = []
         
         for n in range(len(nums)):
+            if n > 0 and nums[n] == nums[n-1]:
+                continue
             l,r = n+1, len(nums)-1 
             while l < r:
                 if (nums[n]+ nums[l] + nums[r]) > 0:
@@ -12,13 +13,9 @@ class Solution:
                 elif (nums[n]+ nums[l] + nums[r]) < 0:
                     l+= 1
                 elif (nums[n]+ nums[l] + nums[r]) == 0:
-                    y = "".join(str(j) for j in [nums[n], nums[l], nums[r]])
-                    if y not in unique:
-                        res.append([nums[n], nums[l], nums[r]])
-                    unique.add(y)
+                    res.append([nums[n], nums[l], nums[r]])
                     r -= 1
                     l += 1
-#                     prevents duplicate entries in res array
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
        
