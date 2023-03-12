f57f3b1cf30ee3d6bdf1c5d8e016b11ad8a0dbd5
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2023-03-12 01:29:29 -0800

    Time: 1118 ms (38.76%), Space: 24.9 MB (81.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,7 +3,7 @@ class Solution:
         profit = 0
         l= 0 
         r = 1
-        while r > l and r <= len(prices)-1:
+        while r <= len(prices)-1:
 #            if profit
             if prices[l] < prices[r]:
                 profit= max(profit, prices[r]-prices[l])
@@ -12,7 +12,6 @@ class Solution:
                 l = r
 #             right pointer will move +1 every iteration
             r+= 1
-            print(profit)
             
         return profit
            
