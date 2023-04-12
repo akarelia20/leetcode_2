6005de3b5619d85927c11d107e5468b78bbc2281
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2023-04-11 18:26:57 -0700

    Time: 808 ms (12.44%), Space: 27.9 MB (86.89%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-#        using the numbers of an array as an pointer
+# using the numbers of an array as an pointer
 # example = [1,3,4,2,2] meaning 1 points at post1(val3), 3 points at pos3(val2), 4 points at pos4(val2), 2 points at pos2(4) and 2 points at pos2(4). 
 # which creates cyclis list so solving this problem in a similer way as cyclic linked list with fast and slow pointers
         slow, fast, slow2 = 0, 0, 0
