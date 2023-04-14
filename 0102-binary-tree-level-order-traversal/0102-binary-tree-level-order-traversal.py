87072ca8b46ef765be5ea3b8c64e3c0c3f829d95
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2023-04-13 23:40:02 -0700

    Time: 43 ms (16.29%), Space: 14.9 MB (16.36%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,26 +6,42 @@
 #         self.right = right
 import collections
 
+# recursive
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res= []
-        q = collections.deque()
-        q.append(root)
-        
-        # using BFS
-        while q:
-            sub_arr = []
-            for i in range(len(q)):
-                node = q.popleft()
-#                 non empty node append to sub_arr
-#                  append righ and left node to queue
-                if node:
-                    sub_arr.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-            if sub_arr:
-                res.append(sub_arr)
+        res = []
+        self.dfs(root, 0, res)
         return res
+        
+    def dfs(self, root, level, res):
+        if not root:
+            return 0
+        if level +1 > len(res):
+            res.append([])
+        res[level].append(root.val)
+        self.dfs(root.left, level+1, res)
+        self.dfs(root.right, level+1, res)
+        
+########### BFS ############    
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         res= []
+#         q = collections.deque()
+#         q.append(root)
+        
+#         # using BFS
+#         while q:
+#             sub_arr = []
+#             for i in range(len(q)):
+#                 node = q.popleft()
+# #                 non empty node append to sub_arr
+# #                  append righ and left node to queue
+#                 if node:
+#                     sub_arr.append(node.val)
+#                     q.append(node.left)
+#                     q.append(node.right)
+#             if sub_arr:
+#                 res.append(sub_arr)
+#         return res
                     
                 
         
