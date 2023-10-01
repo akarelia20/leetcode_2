4523bec6f333ed7fa3102ea2c9b0431799ce3c4e
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2023-10-01 15:06:27 -0700

    Time: 42 ms (43.18%), Space: 14.2 MB (23.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,17 +1,18 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-#         solving using sliding window r,l pointers
-        char= set()
-        l = 0
-        longest= 0
-        
-        for r in range(len(s)):
-#             if letter in set than remove everything before this cur position
-            while s[r] in char:
-                char.remove(s[l])
-                l+=1
-            char.add(s[r])
-            longest = max(longest, r-l+1)
-        return longest
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        output= 0
+        i = 0
+        pos = {}
+        for j in range(len(s)):
+             # if already there you check wheather i == value of s[j] meaning pos of i and previous position of repeating letter is same in that case new value of i = i+1(move one char over)
+            if s[j] in pos:
+                if i == pos[s[j]]:
+                    i += 1
+                 # move i to the previously recoreded position for the letter
+                else:
+                    i = max(pos[s[j]]+1, i)
+            pos[s[j]] = j
+            output = max(output, (j-i)+1)
             
+        return output
                 
