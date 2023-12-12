db9ede7cfb968894c9f89a3736c0d13e823544e9
Amee Karelia <97579245+akarelia20@users.noreply.github.com>
2023-12-12 13:25:46 -0800

    Time: 496 ms (20.04%), Space: 31 MB (43.51%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        unique= set()
-        for i in nums:
-            if i in unique:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+#         create set which will contain unique val
+        unique = set()
+# loop through list and add vals to "unique" set and if the value already exsist return true
+        for num in nums:
+            if num in unique:
                 return True
-            
-            unique.add(i)
+            unique.add(num)
         return False
